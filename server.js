9a7db0b679ebf3f430547ca64ca246bdec03c769
Jonathan Bower <bowerpower85@gmail.com>
2020-09-14 17:01:25 -0700

    Added my html get requests and my port listener.

--- server.js
+++ server.js
@@ -24,3 +24,57 @@ app.get ('/api/notes', function(err, res) {
     };
     res.json(currentNote);
 });
+
+app.post('/api/notes', function(req, res) {
+    try{
+        currentNote = fs.readFileSync('./db/db.json', 'utf-8');
+        console.log(currentNote);
+
+        currentNote = JSON.parse(currentNote);
+        req.body.id = currentNote.length;
+        currentNote.push(req.body);
+        currentNote = JSON.stringify(currentNote);
+        fs.writeFile('./db/db.json', currentNote, 'utf-8', function(err) {
+            if (err) throw err;
+        });
+        res.json(JSON.parse(currentNote));
+
+    } catch (err) {
+        throw err;
+        console.error(err);
+    }
+});
+
+app.delete('/api/notes/:id', function(req, res) {
+    try {
+        currentNote = fs.readFileSync('./db/db.json', 'utf-8');
+        currentNote = JSON.parse(currentNote);
+        currentNote = currentNote.filter(function(note) {
+           return note.id != req.params.id; 
+        });
+        currentNote = JSON.stringify(currentNote);
+        fs.writeFile('./db/db.json', currentNote, 'utf-8', function(err) {
+            if (err) throw err;
+        });
+        res.send(JSON.parse(currentNote));
+    } catch (err) {
+        throw err;
+        console.log(err);
+    }
+});
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/notes/:id', function(req, res) {
+    return res.sendFile(path.json(__dirname, 'db/db.json'));
+});
+
+app.listen(PORT, function() {
+    console.log('App listening on http://localhost:' + PORT);
+});
